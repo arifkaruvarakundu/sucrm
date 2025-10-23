2cff5b3ae2df866353337e83ae7b264683b5c27d
harif <harifn786@gmail.com>
2025-10-23 14:14:24 +0300

    frontend subdomain name is added to the api_config

--- frontend/api_config.js
+++ frontend/api_config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://165.22.82.109";
+const API_BASE_URL = "https://api.sucrm.souqalsultan.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
